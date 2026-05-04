a8fd1ffb60499a633a190375caee61186692814d
lmoeller12 <lmoeller4@gmail.com>
2026-05-03 23:17:53 -0400

    Governance page updates and additons to script file and css sheet

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ const navLinks = document.querySelector(".nav-links");
 navToggle.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+
+const select = document.getElementById('language-select');
+const docLink = document.getElementById('doc-link');
+
+if (select && docLink) {
+  select.addEventListener('change', () => {
+    docLink.href = select.value;
+  });
+}
+
+const pbppSelect = document.getElementById('pbpp-language-select');
+const pbppLink = document.getElementById('pbpp-doc-link');
+
+if (pbppSelect && pbppLink) {
+  pbppSelect.addEventListener('change', () => {
+    pbppLink.href = pbppSelect.value;
+  });
+}
